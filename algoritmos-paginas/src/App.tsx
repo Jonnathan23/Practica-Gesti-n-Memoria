f3d43935896acf23384c69444ff3b30bff49732f
Jonnathan23 <jsaquicela26@gmail.com>
2024-06-26 12:03:32 -0500

    Planteamiento de las cosas que hay que hacer

--- algoritmos-paginas/src/App.tsx
+++ algoritmos-paginas/src/App.tsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <title>Practica</title>
+      </header>
+      <main>
+        <form action="">
+          <fieldset>
+            <legend>Datos Iniciales</legend>
+
+            <div className="camp">
+              <label htmlFor="number_pages">Número de referencias </label>
+              <input type="text" name="number_pages" id="number_pages" placeholder='Números de referencias de páginas' />
+            </div>
+
+            <div className="campo">
+              <label htmlFor="number_pages">Números de marcos de la RAM</label>
+              <input type="text" name="number_pages" id="number_pages" placeholder='Números de marcos de la RAM' />
+            </div>
+            
+            {
+            /**
+             * TODO: Colocar en un bulce para generar el número de referencias
+             * @params numero de referencias
+             */             
+            }
+            
+            {
+              /**
+               * TODO: Metodo: FCFS (primera página en entrar, primera en salir)
+               */
+            }
+
+            {
+              /**
+               * TODO: Metodo: OPTIMO (se reemplaza la página que no se utilizará en más tiempo)
+               */
+            }
+
+            {
+              /**
+               * TODO: Metodo: LRU (se reemplaza la página menos recientemente utilizada)
+               */
+            }
+          </fieldset>
+        </form>
+      </main>
     </>
   )
 }
