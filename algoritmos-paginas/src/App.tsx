bd7e4c07634baaa7e48830844004edef7afd98c2
Jonnathan23 <jsaquicela26@gmail.com>
2024-06-27 16:31:37 -0500

    Diseño del formulario terminado

--- algoritmos-paginas/src/App.tsx
+++ algoritmos-paginas/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -11,58 +11,57 @@ function App() {
       </header>
 
       <main>
-        <form action="">
-          <fieldset>                       
-            <div className="cont__camp">
-              <div className="camp">
-                <svg xmlns="http://www.w3.org/2000/svg" width="274" height="568" fill="none" viewBox="0 0 274 568">
-                  <rect width="274" height="568" fill="#483EFF" rx="10" />                  
-                  <mask id="a" width="274" height="568" x="0" y="0" maskUnits="userSpaceOnUse">
-                    <rect width="274" height="568" fill="#fff" rx="10" />
-                    
-                  </mask>
-                  <g mask="url(#a)">
-                    <path fill="#6259FF" fill-rule="evenodd" d="M-34.692 543.101C3.247 632.538 168.767 685.017 211.96 612.52c43.194-72.497-66.099-85.653-104.735-160.569-38.635-74.916-68.657-121.674-124.482-104.607-55.824 17.068-55.375 106.32-17.436 195.757Z" clip-rule="evenodd" /><path fill="#F9818E" fill-rule="evenodd" d="M233.095 601.153c60.679-28.278 92.839-143.526 41.875-171.528-50.965-28.003-57.397 47.579-108.059 75.987-50.662 28.408-82.14 50.207-69.044 88.241 13.096 38.034 74.549 35.578 135.228 7.3Z" clip-rule="evenodd" /><path stroke="#fff" stroke-linecap="round" stroke-linejoin="bevel" stroke-width="5" d="m165.305 469.097 10.607-10.806M209.461 474.581l-12.506-10.503M187.56 488.991l-6.908 14.798" /><path fill="#FFAF7E" d="M.305 546.891c37.003 0 67-29.997 67-67s-29.997-67-67-67-67 29.997-67 67 29.997 67 67 67Z" />
-                  </g>
-                </svg>
+        <div className="background__camp">
+          <form action="">
+            <fieldset>
+              <div className="cont__camp">
+                <div className="camp__cont__img">
+                  <svg className="camp__img" xmlns="http://www.w3.org/2000/svg" width="274" height="568" fill="none" viewBox="0 0 274 568">
+                    <rect width="274" height="568" fill="#483EFF" rx="10" />
+                    <mask id="a" width="274" height="568" x="0" y="0" maskUnits="userSpaceOnUse">
+                      <rect width="274" height="568" fill="#fff" rx="10" />
 
+                    </mask>
+                    <g mask="url(#a)">
+                      <path fill="#6259FF" fill-rule="evenodd" d="M-34.692 543.101C3.247 632.538 168.767 685.017 211.96 612.52c43.194-72.497-66.099-85.653-104.735-160.569-38.635-74.916-68.657-121.674-124.482-104.607-55.824 17.068-55.375 106.32-17.436 195.757Z" clip-rule="evenodd" />
+                      <path fill="#F9818E" fill-rule="evenodd" d="M233.095 601.153c60.679-28.278 92.839-143.526 41.875-171.528-50.965-28.003-57.397 47.579-108.059 75.987-50.662 28.408-82.14 50.207-69.044 88.241 13.096 38.034 74.549 35.578 135.228 7.3Z" clip-rule="evenodd" />
+                      <path stroke="#fff" stroke-linecap="round" stroke-linejoin="bevel" stroke-width="5" d="m165.305 469.097 10.607-10.806M209.461 474.581l-12.506-10.503M187.56 488.991l-6.908 14.798" />
+                      <path fill="#FFAF7E" d="M.305 546.891c37.003 0 67-29.997 67-67s-29.997-67-67-67-67 29.997-67 67 29.997 67 67 67Z" />
+                    </g>
+                  </svg>
+                </div>
+
+                <div className="camp__inputs">
+
+                  <section className="camp__text">
+                    <h3 className="camp__title">Datos de entrada</h3>
+                    <p className="camp__p">Ingrese los datos necesarios para realizar el cálculo de los algoritmos por favor</p>
+                  </section>
+                  <div className="camp">
+                    <label htmlFor="number_pages">Número de referencias </label>
+                    <input className="camp__txt" type="number" name="number_pages" id="number_pages" placeholder='Ej: 7' />
+                  </div>
+
+                  <div className="camp">
+                    <label htmlFor="number_frames">Números de marcos de la RAM</label>
+                    <input className="camp__txt" type="number" name="number_frames" id="number_frames" placeholder='Ej: 3' />
+                  </div>
+
+                  <div className="camp references">
+                    <label htmlFor="references">Referencias</label>
+                    <textarea className="camp__txtarea" name="references" id="references" rows={1} cols={10} placeholder="Ej: 2, 4, 6, ..."></textarea>
+                  </div>
+                </div>
               </div>
 
-              <div className="camp__inputs">
-
-                <section className="camp__title">
-                  <h3>Datos de entrada</h3>
-                  <p>Ingrese los datos necesarios para realizar el cálculo de los algoritmos por favor</p>
-                </section>
-                <div className="camp">
-                  <label htmlFor="number_pages">Número de referencias </label>
-                  <input type="text" name="number_pages" id="number_pages" placeholder='Números de referencias de páginas' />
-                </div>
-
-                <div className="camp">
-                  <label htmlFor="number_frames">Números de marcos de la RAM</label>
-                  <input type="text" name="number_frames" id="number_frames" placeholder='Números de marcos de la RAM' />
-                </div>
-
-                <div className="camp">
-                  {
-                    /**
-                     * TODO: Colocar en un bulce para generar el número de referencias
-                     * @params numero de referencias
-                     */
-                  }
-                </div>
-              </div>
-            </div>
+            </fieldset>
+          </form>
+        </div>
 
 
-
-          </fieldset>
-        </form>
-
-        <div className="algoritms__background">
+        <div id="algoritms_id" className="algoritms__background algoritms__show">
           <div className="algoritms__content">
-            <section className="algoritms">
+            <section className="algoritms background__camp">
               {
                 /**
                  * TODO: Metodo: FCFS (primera página en entrar, primera en salir)
@@ -71,7 +70,7 @@ function App() {
 
             </section>
 
-            <section className="algoritms">
+            <section className="algoritms background__camp">
               {
                 /**
                  * TODO: Metodo: OPTIMO (se reemplaza la página que no se utilizará en más tiempo)
@@ -80,7 +79,7 @@ function App() {
 
             </section>
 
-            <section className="algoritms">
+            <section className="algoritms background__camp">
               {
                 /**
                  * TODO: Metodo: LRU (se reemplaza la página menos recientemente utilizada)
